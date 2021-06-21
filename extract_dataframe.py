8f4e171680075e9e15ac685cb71ddd3a6449907b
DiboraHaile <deborahaile3@gmail.com>
2021-06-21 20:53:40 +0300

    renamed files

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -35,7 +35,8 @@ class TweetDfExtractor:
 
     # an example function
     def find_statuses_count(self)->list:
-        statuses_count 
+        statuses_count = [self.tweets_list[i]['user']['statuses_count'] for i in range(len(self.tweets_list))]
+        return statuses_count
         
     def find_full_text(self)->list:
         text = 
