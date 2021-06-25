45313b1f5a1274b9ce0250c4109207bad1f13c6d
DiboraHaile <deborahaile3@gmail.com>
2021-06-26 01:20:23 +0300

    corrected the error for the markdown command

--- Db_files/streamlit.py
+++ Db_files/streamlit.py
@@ -4,7 +4,7 @@ import pandas as pd
 import query
 
 
-st.write("<h1 style='color: gray;'>Twitter Data</h1>")
+st.markdown("<h1 style='color: gray;'>Twitter Data</h1>", unsafe_allow_html=True)
 df = query.fetch_data('twitter_data')
 # st.markdown("<p style='padding:10px; background-color:#000000;color:#00ECB9;font-size:16px;border-radius:10px;'>Section Break</p>", unsafe_allow_html=True)
 st.write(df)
