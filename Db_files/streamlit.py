b7f2a075f25c9aa754f21c378fc43ba3c811c5b7
DiboraHaile <deborahaile3@gmail.com>
2021-06-26 23:46:39 +0300

    added wordcloud visualization

--- Db_files/streamlit.py
+++ Db_files/streamlit.py
@@ -2,22 +2,32 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import query
+import wordcloud
+from wordcloud import WordCloud
 
-pages = ['Display Data Frame ','Visualize ']
+
 def display_df():
     df = query.fetch_data('twitter_data')
     return df
 
+# def search_bar():
+
 def toggle_bn_pages():
+    pages = ['Display Data Frame ','Visualize ']
     option = st.sidebar.selectbox('Choose:',pages)
-    return option
-
-page = toggle_bn_pages()
-if page == pages[0]:
-    st.markdown("<h1 style='color: gray;'>"+page+"of Twitter Data</h1>", unsafe_allow_html=True)
     df = display_df()
-    st.write(df)
+    if option == pages[0]:
+        st.markdown("<h1 style='color: gray;'>"+option+"of Twitter Data</h1>", unsafe_allow_html=True)
+        st.write(df)
 
-else:
-    st.markdown("<h1 style='color: gray;'>"+page+"Twitter Data</h1>", unsafe_allow_html=True)
-    st.write('visualization here')
+    else:
+        st.markdown("<h1 style='color: gray;'>"+option+"Twitter Data</h1>", unsafe_allow_html=True)
+        st.write('visualization here')
+        text = ""
+        for rows in df["original_text"]:
+            text += rows
+        wordcloud = WordCloud().generate(text)
+        st.image(wordcloud.to_array())
+
+
+toggle_bn_pages()
