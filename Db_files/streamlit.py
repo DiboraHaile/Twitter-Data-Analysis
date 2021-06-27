2043a7eb98358881e05cc69414cebb1c16c8957a
DiboraHaile <deborahaile3@gmail.com>
2021-06-27 15:09:46 +0300

    added visualization and display for raw and cleaned data

--- Db_files/streamlit.py
+++ Db_files/streamlit.py
@@ -6,23 +6,30 @@ import wordcloud
 from wordcloud import WordCloud
 
 
-def display_df():
-    df = query.fetch_data('twitter_data')
+def display_df(table_name):
+    df = query.fetch_data(table_name)
     return df
 
 # def search_bar():
 
 def toggle_bn_pages():
     pages = ['Display Data Frame ','Visualize ']
+    data = ['Clean Data','Raw Data']
     option = st.sidebar.selectbox('Choose:',pages)
-    df = display_df()
+    display_data_option = st.sidebar.selectbox('Display:',data)
+    if display_data_option == data[0]:
+            df = display_df('twitter_data1')
+    else:
+        df = display_df('twitter_data')
+
     if option == pages[0]:
         st.markdown("<h1 style='color: gray;'>"+option+"of Twitter Data</h1>", unsafe_allow_html=True)
+        st.markdown("<h2 style='color: gray;'>"+display_data_option+"</h2>", unsafe_allow_html=True)
         st.write(df)
 
     else:
         st.markdown("<h1 style='color: gray;'>"+option+"Twitter Data</h1>", unsafe_allow_html=True)
-        st.write('visualization here')
+        st.markdown("<h2 style='color: gray;'>"+display_data_option+"</h2>", unsafe_allow_html=True)
         text = ""
         for rows in df["original_text"]:
             text += rows
