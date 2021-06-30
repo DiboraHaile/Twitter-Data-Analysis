af323d5dd75f4647f96e6e3cc482abcd02f01a33
DiboraHaile <deborahaile3@gmail.com>
2021-06-30 18:59:38 +0300

    modularized code and fixed errors

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -177,9 +177,9 @@ if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
     # columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     # 'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    # _, tweet_list = read_json("data/covid19.json")
-    # tweet = TweetDfExtractor(tweet_list)
+    _, tweet_list = read_json("data/covid19.json")
+    tweet = TweetDfExtractor(tweet_list)
     # tweet_full = tweet.find_full_text()
-    # tweet_df = tweet.get_tweet_df()
+    tweet_df = tweet.get_tweet_df()
 
     # print(tweet_df['lang'][:25])
